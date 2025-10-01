66d3831a09b70df9cf30f9d1a03f9c0231323142
Vadim (Dury) Gus <vadim@guslistov.com>
2025-10-01 17:37:40 +0300

    Convert: models/tag.js в чистый ES6

--- src/models/tag.js
+++ src/models/tag.js
@@ -12,11 +12,8 @@
  *  Copyright (c) 2014-2019. All rights reserved.
  */
 
-import { createRequire } from 'module'
-const require = createRequire(import.meta.url)
-
-var mongoose = require('mongoose')
-var utils = require('../helpers/utils')
+import mongoose from 'mongoose'
+import utils from '../helpers/utils.js'
 
 var COLLECTION = 'tags'
 
