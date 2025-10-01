e434c46aed44724641935fcd5ac11916af132ec5
Vadim (Dury) Gus <vadim@guslistov.com>
2025-10-01 16:17:47 +0300

    Fix: Заменены все moment на dayjs в ticketStats.js

--- src/cache/ticketStats.js
+++ src/cache/ticketStats.js
@@ -29,7 +29,7 @@ function buildGraphData (arr, days, callback) {
   if (arr.length < 1) {
     return callback(graphData)
   }
-  const today = moment()
+  const today = dayjs()
     .hour(23)
     .minute(59)
     .second(59)
@@ -39,7 +39,7 @@ function buildGraphData (arr, days, callback) {
   }
 
   arr = _.map(arr, function (i) {
-    return moment(i.date).format('YYYY-MM-DD')
+    return dayjs(i.date).format('YYYY-MM-DD')
   })
 
   let counted = _.countBy(arr)
@@ -67,8 +67,8 @@ function buildAvgResponse (ticketArray, callback) {
     const ticket = ticketArray[i]
     if (ticket.comments === undefined || ticket.comments.length < 1) continue
 
-    const ticketDate = moment(ticket.date)
-    const firstCommentDate = moment(ticket.comments[0].date)
+    const ticketDate = dayjs(ticket.date)
+    const firstCommentDate = dayjs(ticket.comments[0].date)
 
     const diff = firstCommentDate.diff(ticketDate, 'seconds')
     $ticketAvg.push(diff)
@@ -82,7 +82,7 @@ function buildAvgResponse (ticketArray, callback) {
     0
   )
 
-  const tvt = moment.duration(Math.round(ticketAvgTotal / _.size($ticketAvg)), 'seconds').asHours()
+  const tvt = dayjs.duration(Math.round(ticketAvgTotal / _.size($ticketAvg)), 'seconds').asHours()
   cbObj.avgResponse = Math.floor(tvt)
 
   return callback(cbObj)
@@ -96,8 +96,8 @@ const init = function (tickets, callback) {
   ex.e180 = {}
   ex.e365 = {}
   ex.lifetime = {}
-  ex.lastUpdated = moment.utc()
-  const today = moment()
+  ex.lastUpdated = dayjs.utc()
+  const today = dayjs()
     .hour(23)
     .minute(59)
     .second(59)
